56f022c73c060fe83aff2d9cc99c9e604963c4d6
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-02-24 20:24:32 +0000

    exercicio de calculos

---
+++ src/components/Calculo.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useState } from "react";
+
+const Calculo = () => {
+
+const  [num1, setNum1] = useState(0);
+const  [num2, setNum2] = useState(0);
+const  [total, setTotal] = useState();
+
+const handleSoma = () => {
+    let soma = parseInt(num1) + parseInt(num2);
+    setTotal(soma);
+}
+
+const handleDiminui = () => {
+    let diminuir = parseInt(num1) - parseInt(num2)
+    setTotal(diminuir);
+}
+
+const handleMultiplica = () => {
+    let multiplicar = parseInt(num1) * parseInt(num2)
+    setTotal(multiplicar)
+}
+
+const handleDividi = () => {
+    let dividir = parseInt(num1) / parseInt(num2)
+    setTotal(dividir)
+}
+
+
+
+    return(
+        <>
+            <h2>Faça seu cálculo</h2>
+            <input type="text" 
+            placeholder="Digite um número"
+            onChange={(e) => setNum1(e.target.value)}/>
+            <input type="text" 
+            placeholder="Digite um número"
+            onChange={(e) => setNum2(e.target.value)}/>
+
+        <div>
+    
+                <button onClick={handleSoma}>+</button>
+                <button onClick={handleDiminui}>-</button>
+                <button onClick={handleMultiplica}>*</button>
+                <button onClick={handleDividi}>/</button>
+        </div>
+
+            <h3>Total: {total}</h3>
+        </>
+    )
+}
+
+export default Calculo;
