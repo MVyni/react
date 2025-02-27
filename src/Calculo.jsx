f4dbae937a86be2bd7e8f42ddc142a00840168f1
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-02-27 22:22:19 +0000

    inserindo exibição historico

--- src/Calculo.jsx
+++ src/Calculo.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import useAtualize from "./hooks/useAtualize";
-import useHistoric from "./hooks/useHistorico";
 
 
 const Calculo = () => {
@@ -9,10 +8,13 @@ const Calculo = () => {
 const [num1, setNum1] = useState(0);
 const [num2, setNum2] = useState(0);
 const [total, setTotal] = useState();
+const [historico, setHistorico] = useState([]);
 
 
-useAtualize(total);
-
+useEffect(updateHistoric, [total])
+function updateHistoric() {
+    setHistorico(total)
+}
 
 
 const handleSoma = () => {
